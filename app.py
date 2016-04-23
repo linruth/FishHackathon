ed5dc9b398a4ac9dafb18e02982558094429f372
Lauren <lauren.e.coombe@gmail.com>
2016-04-23 12:47:24 -0700

    Added test post requests for Firebase

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
 from xml.etree.ElementTree import fromstring
+from firebase import firebase
 
 from flask import Flask, render_template, request,json,jsonify, redirect
 import urllib
 
+import random
+import string
+
 app = Flask(__name__)
+firebase = firebase.FirebaseApplication("https://boiling-torch-8247.firebaseio.com/#-KG09txqVpTw6jUmqcas|c3c8dd314bcafecb0cf13741fe2b0825", None)
+
+def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
+	return ''.join(random.choice(chars) for _ in range(size))
 
 @app.route('/')
-def hello_world():
-    print("hello");
+def homepage():
     return render_template("index.html");
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+	id = id_generator()
+	route = "/" + id
+	result = firebase.get(route, None)
+	while result != None:
+		id = id_generator()
+		route = "/" + id
+		result = firebase.get(route, None)
+
+	user = "Lauren"
+	route = "/" + id
+	firebase.put(route, "Item", "Net")
+	firebase.put(route, "Animals", "Fish")
+	firebase.put(route, "Depth", None)
+	firebase.put(route, "Recovered", False)
+	firebase.put(route, "Material", None)
+	return render_template("index.html");
 
 
 if __name__ == '__main__':
